0635ac8f291345b1a98bcb617a6c194515663c45
Ankita <itheankita@gmail.com>
2020-03-22 01:13:40 +0530

    code updated with minor bug fix

--- app/src/main/java/com/example/nasapicturesapp/view/DetailedViewActivity.kt
+++ app/src/main/java/com/example/nasapicturesapp/view/DetailedViewActivity.kt
@@ -1,21 +1,23 @@
 package com.example.nasapicturesapp.view
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.nasapicturesapp.R
 import com.example.nasapicturesapp.data.model.DataModel
+import com.squareup.picasso.Picasso
 
 class DetailedViewActivity : AppCompatActivity() {
     private var mDataModel: DataModel? = null
+    private var mIvPicture: ImageView? = null
     private var mTvTitle: TextView? = null
     private var mTvExplanation: TextView? = null
     private var mTvCopyright: TextView? = null
     private var mTvDate: TextView? = null
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detailed_view)
         mDataModel = intent.getSerializableExtra("Data") as DataModel
         setInitViews()
@@ -23,6 +25,7 @@ class DetailedViewActivity : AppCompatActivity() {
     }
 
     private fun setInitViews() {
+        mIvPicture = findViewById(R.id.iv_picture)
         mTvTitle = findViewById(R.id.tv_title)
         mTvExplanation = findViewById(R.id.tv_explanation)
         mTvCopyright = findViewById(R.id.tv_copyright)
@@ -30,6 +33,10 @@ class DetailedViewActivity : AppCompatActivity() {
     }
 
     private fun setEventListeners() {
+        Picasso.get().load(mDataModel!!.getHdurl())
+                .error(R.mipmap.ic_launcher)
+                .placeholder(R.mipmap.ic_launcher)
+                .into(mIvPicture)
         mTvTitle?.text = mDataModel?.getTitle()
         mTvExplanation?.text = mDataModel?.getExplanation()
         mTvCopyright?.text = mDataModel?.getCopyright()
